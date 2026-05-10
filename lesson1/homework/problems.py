b9f4f1fff0c94c77600a0d727c76bae7f907bfdd
anay-sane <vruke.sane@gmail.com>
2026-05-10 01:10:10 +0000

    HW 1

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,25 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
-
+Myluckynumber = 24
+print(Myluckynumber)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
-
+Sleep = 8
+print(Sleep)
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
-
+favfruit = "alphanso mango"
+print("my favorate fruit is the", favfruit)
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
-
+city = "sammamish" 
+countery = "USA"
+print(city)
+print(countery)
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
