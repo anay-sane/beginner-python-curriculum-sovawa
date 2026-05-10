5881971c5719836eb5dac78ef0fa9e8b1c9da152
anay-sane <vruke.sane@gmail.com>
2026-05-10 01:01:35 +0000

    finished lesson 1

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,5 @@
+print("Hello World")
+print("my name is Anay")
+print ('My favorate food is pizza')
+
+#print("I love dogs")
---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,9 @@
+x= 5 # interger: whole number
+print(x)
+
+number = 0.125 #float: number with a decimal point
+print(number)
+
+favorate_food = "pizza"
+print ("my favorate food is", favorate_food)
+print(4.21, "dogs", x)
